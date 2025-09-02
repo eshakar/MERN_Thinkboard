822ccd53b57259518dcd26649957be026ed5a92d
eshakarpm <esha.kar@peoplemaketh.com>
2025-09-02 21:27:42 +0530

    fourth commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { getToken } from "./auth";
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 const api = axios.create({
-  baseURL: "http://localhost:5001/api"
+  baseURL: BASE_URL
 });
 
 api.interceptors.request.use((cfg) => {
